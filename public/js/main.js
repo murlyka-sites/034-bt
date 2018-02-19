6a111daea48e992dd942d0ad8e54cd3488eca57e
Milok <milok.murlika@yandex.ua>
2018-02-19 15:43:46 +0200

    ✅ характеристики к карточке товара

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,92 @@
+let app = {
+	width: screen.width
+}
+
+
+window.addEventListener('resize', function(event){
+  app.width = screen.width
+});
+
+Vue.component('card-catalog', {
+	props: {
+		image: String,
+		title: String,
+		link: String,
+		price: String,
+		tile: Boolean,
+		mods: Object
+	},
+	methods: {
+		mod: function (cls) {
+			let result = [];
+
+			for (var key in this.mods) {
+			  if(this.mods[key] == true) {
+			  	result.push(cls + '_' +key);
+			  }
+			}
+
+			return result;
+		}
+	},
+	template: `
+	<div class='c-catalog' :class="mod('c-catalog')">
+		<header class="c-catalog__header" :class="mod('c-catalog__header')">
+			<a :href="link">
+				<img class="c-catalog__pr image-responsive" :class="mod('c-catalog__pr')" :src="image" :alt="title"/>
+			</a>
+		</header>
+		<div class="c-catalog__body" :class="mod('c-catalog__body')">
+			<div class="c-catalog__title" :class="mod('c-catalog__title')">
+				<a :href="link" class="c-catalog__title-inner" :class="mod('c-catalog__title-inner')">
+					{{ title }}
+				</a>
+			</div>
+			<div class="c-catalog__chars" :class="mod('c-catalog__chars')">
+				<div class="c-catalog__chars-inner" :class="mod('c-catalog__chars-inner')">
+					<slot></slot>
+				</div>
+			</div>
+		</div>
+
+		<footer class="c-catalog__foo" :class="mod('c-catalog__foo')">
+			<div class="c-catalog__price" :class="mod('c-catalog__price')">{{price}}</div>
+			<button class="button button_type_button button_size_md button_theme_yellow w-160 button_round_5 c-catalog__button" :class="mod('c-catalog__button')">
+				<svg class="button__icon icon" width="16" height="16">
+					<use xlink:href="#busket"></use>
+				</svg>
+				<span class="button__text">В корзину</span> 
+			</button>							
+		</footer>
+	</div>
+
+	`
+});
+
 let vm = new Vue({
 	el: '#app',
 	data: {
+		app: app,
 		topMenuCatalog: 0,
-		asideMenuCatalog: false
+		asideMenuCatalog: false,
+		tabs: {
+			chars: (screen.width < 768) ? 0 : 1,
+		}
+	},
+	computed: {
+		asideCardCatalogMods: function () {
+			return {
+				'tile': (this.width <= 992)
+			}
+		}
+	},
+	methods: {
+		hasActiveTab: function(name, id) {
+			return this.tabs[name] == id
+		},
+		toActiveTab: function(name, id) {
+			this.tabs[name] = id
+		}
 	}
 });
 
